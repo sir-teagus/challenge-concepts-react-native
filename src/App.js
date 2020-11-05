5a08b696cd605b8140c941d2cd6e9e7fd85c4148
sir-teagus <teagus@gmail.com>
2020-11-05 00:24:15 -0300

    Challenge solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,104 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api'
+
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  // useEffect(() => {
+  //   const tempRepositories = [
+  //     {
+  //       id: "3b4592c4-265b-4b04-b5ee-b2f15a63cba6",
+  //       title: "Mobile com React Native",
+  //       url: "http:teste.de.url",
+  //       techs: [
+  //         "React Native",
+  //         "Axios",
+  //       ],
+  //       likes: 0
+  //     },
+  //     {
+  //       id: "b423b938-3c62-4555-b7cc-cadbb68e83f1",
+  //       title: "Frontend com ReactJS",
+  //       url: "http:teste.de.url",
+  //       techs: [
+  //         "ReactJS",
+  //         "Express",
+  //         "Cors"
+  //       ],
+  //       likes: 0
+  //     }
+  //   ]
+  
+  //   setRepositories(tempRepositories)
+  // }, [])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })}, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+
+    const response = await api.post(`repositories/${id}/like`)
+
+    const likes = response.data.likes
+
+    const updatedRepositories = repositories.map(repository => {
+      if (repository.id == id) {
+        return { ...repository, likes}
+      } else {
+        return repository
+      }
+    })
+
+    setRepositories(updatedRepositories)
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+        <FlatList 
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <View style={styles.techsContainer}>
+                
+                {repository.techs.map(tech => (
+                  <Text key={repository.tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                ))}
+
+              </View>
+
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} curtidas
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+
       </SafeAreaView>
     </>
   );
